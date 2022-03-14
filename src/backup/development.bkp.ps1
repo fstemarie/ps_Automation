4a6681ea48cb1947aa47f17c80358506734c2aa4
fstemarie <fstemarie@gmail.com>
2022-03-14 13:34:06 -0400

    Added backup of development folder

---
+++ src/backup/development.bkp.ps1
@@ -0,0 +1,35 @@
+#-----------------------------------------------------------------------
+#region Sauvegarde du dossier development sur Raktar
+Write-Host ("-" * 49)
+Write-Host "| Sauvegarde du dossier documents sur Raktar |"
+Write-Host ("-" * 49)
+
+$source = "D:\Francois\Documents\Development"
+$dest = "\\raktar\Storage\backup\development"
+# Creation du fichier incremental
+$arch = "$dest\development.7z"
+$incr = "$dest\development.$(Get-Date -Format FileDateTime).7z"
+if (Test-Path -Path $arch -PathType Leaf) {
+    $params = @(
+        "-u-"
+        "-up1q1r3x1y1z0w1!$incr"
+        "-xr!node_modules"
+        "-xr!.venv"
+        "-mx=9"
+        $arch
+        $source
+    )
+    7z u @params
+}
+
+# Mise a jour de la sauvegarde complete
+$params = @(
+    "-up0q0r2x2y2z1w2"
+    "-mx=9"
+    "-xr!node_modules"
+    "-xr!.venv"
+    $arch
+    $source
+)
+7z u @params
+#endregion
