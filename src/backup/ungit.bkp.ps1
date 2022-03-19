e6bc77a9d6b9fe7216b1e119026a601ab81b88b7
fstemarie <fstemarie@gmail.com>
2022-03-19 15:12:30 -0400

    Ungit Service

---
+++ src/backup/ungit.bkp.ps1
@@ -0,0 +1,17 @@
+#-----------------------------------------------------------------------
+#region Sauvegarde de la configuration de Node-RED sur Raktar
+Write-Host ("-" * 53)
+Write-Host "| Sauvegarde de la configuration de Ungit sur Raktar |"
+Write-Host ("-" * 53)
+
+$source = "D:\services\ungit"
+$arch = "\\raktar\Storage\backup\ungit\ungit.7z"
+$params = @(
+    "-up0q0r2x2y2z1w2"
+    "-xr!node_modules"
+    "-mx=9"
+    $arch
+    $source
+)
+7z u @params
+#endregion
