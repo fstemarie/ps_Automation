cc5d72f33f5d567a257213d6dd7f0dfc1b937a6b
fstemarie <fstemarie@gmail.com>
2022-03-19 15:12:13 -0400

    Simplified

--- src/backup/nodered.bkp.ps1
+++ src/backup/nodered.bkp.ps1
@@ -5,8 +5,7 @@ Write-Host "| Sauvegarde de la configuration de Node-RED sur Raktar |"
 Write-Host ("-" * 57)
 
 $source = "D:\services\node-red"
-$dest = "\\raktar\Storage\backup\node-red"
-$arch = "$dest\node-red.7z"
+$arch = "\\raktar\Storage\backup\node-red\node-red.7z"
 $params = @(
     "-up0q0r2x2y2z1w2"
     "-xr!node_modules"
--- src/backup/rclone.bkp.ps1
+++ src/backup/rclone.bkp.ps1
@@ -5,8 +5,7 @@ Write-Host "| Sauvegarde de la configuration de rClone sur Raktar |"
 Write-Host ("-" * 57)
 
 $source = "D:\services\rclone"
-$dest = "\\raktar\Storage\backup\rclone"
-$arch = "$dest\rclone.7z"
+$arch = "\\raktar\Storage\backup\rclone\rclone.7z"
 $params = @(
     "-up0q0r2x2y2z1w2"
     "-x!rclone\rclone.conf.log"
