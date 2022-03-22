5f05b99c3466a4fcf249cc89b7ea330a06053bc6
fstemarie <fstemarie@gmail.com>
2022-03-22 00:32:08 -0400

    Debarasse de la variable $dest

--- src/backup/vimfiles.bkp.ps1
+++ src/backup/vimfiles.bkp.ps1
@@ -5,8 +5,7 @@ Write-Host "| Sauvegarde de la config Vim sur Raktar |"
 Write-Host ("-" * 42)
 
 $source = "D:\Francois\vimfiles"
-$dest = "\\raktar\backup\HX90\vimfiles"
-$arch = "$dest\vimfiles.7z"
+$arch = "\\raktar\backup\HX90\vimfiles\vimfiles.7z"
 $params = @(
     "-up0q0r2x2y2z1w2"
     "-mx=9"
