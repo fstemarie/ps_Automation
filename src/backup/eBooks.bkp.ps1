77187092076753af8222dae31f3d5c01c14a15ec
fstemarie <fstemarie@gmail.com>
2022-03-22 00:29:37 -0400

    Debarrasse de la variable $dest

--- src/backup/eBooks.bkp.ps1
+++ src/backup/eBooks.bkp.ps1
@@ -5,8 +5,7 @@ Write-Host "| Sauvegarde du dossier eBooks sur Raktar |"
 Write-Host ("-" * 43)
 
 $source = "D:\Francois\eBooks"
-$dest = "\\raktar\backup\HX90\eBooks"
-$arch = "$dest\eBooks.7z"
+$arch = "\\raktar\backup\HX90\eBooks\eBooks.7z"
 $params = @(
     "-mx=9"
     $arch
