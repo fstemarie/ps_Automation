2c6743a5a49e391def2a682129df0a5feeb45dd0
fstemarie <fstemarie@gmail.com>
2022-04-07 01:07:43 -0400

    Use robocopy to backup eBooks instead of tar

---
+++ src/backup/robocopy/eBooks.bkp.ps1
@@ -0,0 +1,16 @@
+Start-Transcript `
+    -Path D:\automation\log\eBooks.robocopy.log `
+    -Append -IncludeInvocationHeader
+
+#-----------------------------------------------------------------------
+#region Sauvegarde du dossier documents sur Raktar
+Write-Host ("-" * 43)
+Write-Host "| Sauvegarde du dossier eBooks sur Raktar |"
+Write-Host ("-" * 43)
+
+$src = "D:\Francois\eBooks"
+$dst = "\\raktar\backup\HX90\eBooks"
+robocopy $src $dst /MIR /M /SL /SJ /MT /NP
+#endregion
+
+Stop-Transcript
---
+++ src/backup/robocopy/eBooks.rec.ps1
@@ -0,0 +1,10 @@
+#-----------------------------------------------------------------------
+#region Sauvegarde du dossier documents sur Raktar
+Write-Host ("-" * 43)
+Write-Host "| Recuperation du dossier eBooks de Raktar |"
+Write-Host ("-" * 43)
+
+$src = "\\raktar\backup\HX90\eBooks"
+$dst = "D:\Francois\eBooks"
+robocopy $src $dst /E /M /ZB /SL /SJ /MT /NP
+#endregion
