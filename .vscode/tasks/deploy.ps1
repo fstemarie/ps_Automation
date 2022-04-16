a41cf8101c7596455f15a02dcae98f5ac7a66380
fstemarie <fstemarie@gmail.com>
2022-04-16 18:54:11 -0400

    added streamlink.ps1 to deploy script

--- .vscode/tasks/deploy.ps1
+++ .vscode/tasks/deploy.ps1
@@ -1,2 +1,3 @@
 Write-Host "Deploying raktar_credentials to D:\automation"
 Copy-Item -Path '.\src\raktar_credentials.ps1' -Destination 'D:\automation'
+Copy-Item -Path '.\src\streamlink.ps1' -Destination 'D:\automation'
