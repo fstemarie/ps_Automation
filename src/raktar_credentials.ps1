814faeca1c93ae96891c3c4be729158a9ca038a2
fstemarie <fstemarie@users.noreply.github.com>
2022-06-25 01:46:03 -0400

    Readded \\raktar as it created problems

--- src/raktar_credentials.ps1
+++ src/raktar_credentials.ps1
@@ -10,3 +10,15 @@ net use \\raktar.local\Storage      /USER:francois theglacer
 net use \\raktar.local\Tangerine    /USER:francois theglacer
 net use \\raktar.local\Videos       /USER:francois theglacer
 net use \\raktar.local\OMV          /USER:francois theglacer
+net use \\raktar\Copper       /USER:francois theglacer
+net use \\raktar\Copper_Audio /USER:francois theglacer
+net use \\raktar\Elements     /USER:francois theglacer
+net use \\raktar\Emerald      /USER:francois theglacer
+net use \\raktar\Mauve        /USER:francois theglacer
+net use \\raktar\Mauve_Audio  /USER:francois theglacer
+net use \\raktar\MyBook       /USER:francois theglacer
+net use \\raktar\Slim         /USER:francois theglacer
+net use \\raktar\Storage      /USER:francois theglacer
+net use \\raktar\Tangerine    /USER:francois theglacer
+net use \\raktar\Videos       /USER:francois theglacer
+net use \\raktar\OMV          /USER:francois theglacer
