dd60383a8d715c733e9fa3dc8005e54f2c74c108
fstemarie <14352349+fstemarie@users.noreply.github.com>
2022-07-31 02:09:48 -0400

    Fixed some parameters

--- src/backup/robocopy/eBooks.rec.ps1
+++ src/backup/robocopy/eBooks.rec.ps1
@@ -1,10 +1,14 @@
 #-----------------------------------------------------------------------
-#region Sauvegarde du dossier documents sur Raktar
-Write-Host ("-" * 43)
+#region Sauvegarde du dossier eBooks sur Raktar
+Write-Host ("-" * 44)
 Write-Host "| Recuperation du dossier eBooks de Raktar |"
-Write-Host ("-" * 43)
+Write-Host ("-" * 44)
 
 $src = "\\raktar.local\backup\HX90\eBooks"
-$dst = "D:\Francois\eBooks"
-robocopy $src $dst /E /M /ZB /SL /SJ /MT /NP
+$dst = "D:\francois\eBooks"
+if (Test-Path -Path $dst -PathType Container) {
+    Move-Item -Path $dst -Destination "$dst.$(Get-Date -UFormat %s)"
+}
+
+robocopy $src $dst /MIR
 #endregion
