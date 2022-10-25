b192540f64dd534822b276903b972ecd20b14118
fstemarie <14352349+fstemarie@users.noreply.github.com>
2022-10-25 01:04:18 -0400

    Does not fetch from google drive for the backup

--- src/backup/7zip/keepass.bkp.ps1
+++ src/backup/7zip/keepass.bkp.ps1
@@ -22,7 +22,8 @@ $params = @(
 
 Push-Location -Path C:\
 7z u @params
-rclone cat google.bb:/keepass/Passwords.kdbx | 7z u $arc -si"Passwords.kdbx"
+
+# rclone cat google.bb:/keepass/Passwords.kdbx | 7z u $arc -si"Passwords.kdbx"
 # rclone cat google:/Services/KeePass/Discarded.kdbx | 7z u $arc -si"Discarded.kdbx"
 Pop-Location
 #endregion
