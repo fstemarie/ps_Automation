c39100d21ebcd5f7f102bbab70fc134f217a7433
fstemarie <14352349+fstemarie@users.noreply.github.com>
2022-12-13 00:30:52 -0500

    Simplified deploy scripts

--- .vscode/tasks/deploy.ps1
+++ .vscode/tasks/deploy.ps1
@@ -1,2 +1,8 @@
 Write-Host "Deploying scripts to D:\automation"
-Copy-Item -Path '.\src\*.ps1' -Destination 'D:\automation'
+
+$dst = 'D:\automation\backup'
+Remove-Item -Force -Recurse $dst -ErrorAction SilentlyContinue
+$src = '.\src'
+Copy-Item -Path (Join-Path $src '*.ps1') -Destination 'D:\automation'
+$src = Join-Path $src 'backup'
+Copy-Item -Path $src -Destination 'D:\automation'
