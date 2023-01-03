702afaf1e79df83a62c9281d792eb500743e16bf
fstemarie <14352349+fstemarie@users.noreply.github.com>
2023-01-02 22:55:54 -0500

    Simplified

--- src/credentials.ps1
+++ src/credentials.ps1
@@ -9,18 +9,15 @@ while (-not $creds) {
     if ($i-- -eq 0) { exit }
     $creds = KeePassEntry "raktar.local - francois"
     Start-Sleep -Seconds 2
-    Write-Host "Here!"
 }
 
-if ($creds) {
-    net use \\raktar.local\audio        /USER:francois $($creds.password)
-    net use \\raktar.local\backup       /USER:francois $($creds.password)
-    net use \\raktar.local\copper       /USER:francois $($creds.password)
-    net use \\raktar.local\elements     /USER:francois $($creds.password)
-    net use \\raktar.local\emerald      /USER:francois $($creds.password)
-    net use \\raktar.local\mybook       /USER:francois $($creds.password)
-    net use \\raktar.local\slim         /USER:francois $($creds.password)
-    net use \\raktar.local\storage      /USER:francois $($creds.password)
-    net use \\raktar.local\tangerine    /USER:francois $($creds.password)
-    net use \\raktar.local\videos       /USER:francois $($creds.password)
-}
+net use \\raktar.local\audio        /USER:francois $($creds.password)
+net use \\raktar.local\backup       /USER:francois $($creds.password)
+net use \\raktar.local\copper       /USER:francois $($creds.password)
+net use \\raktar.local\elements     /USER:francois $($creds.password)
+net use \\raktar.local\emerald      /USER:francois $($creds.password)
+net use \\raktar.local\mybook       /USER:francois $($creds.password)
+net use \\raktar.local\slim         /USER:francois $($creds.password)
+net use \\raktar.local\storage      /USER:francois $($creds.password)
+net use \\raktar.local\tangerine    /USER:francois $($creds.password)
+net use \\raktar.local\videos       /USER:francois $($creds.password)
