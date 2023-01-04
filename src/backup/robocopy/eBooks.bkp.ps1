283b916139d2a31c32952ccfd0d367b69b46456b
fstemarie <14352349+fstemarie@users.noreply.github.com>
2023-01-04 02:44:35 -0500

    Little updates

--- src/backup/robocopy/eBooks.bkp.ps1
+++ src/backup/robocopy/eBooks.bkp.ps1
@@ -1,6 +1,18 @@
-Start-Transcript `
-    -Path D:\automation\log\eBooks.robocopy.log `
-    -Append -IncludeInvocationHeader
+# https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
+$src = "D:\Francois\eBooks"
+$dst = "\\raktar.local\backup\HX90\eBooks"
+
+if (!$env:AUTOMATION -Or !(Test-Path "$env:AUTOMATION")) {
+    Write-Error "eBooks.bkp.ps1 -- AUTOMATION empty or invalid. Cannot proceed"
+    exit 1
+}
+
+$params = @{
+    Path = Join-Path $env:AUTOMATION "log" "eBooks.robocopy.log"
+    Append = $true
+    IncludeInvocationHeader = $true
+}
+Start-Transcript @params
 
 #-----------------------------------------------------------------------
 #region Sauvegarde du dossier documents sur Raktar
@@ -8,8 +20,6 @@ Write-Host "┌─────────────────────�
 Write-Host "│ Sauvegarde du dossier eBooks sur Raktar │"
 Write-Host "└─────────────────────────────────────────┘"
 
-$src = "D:\Francois\eBooks"
-$dst = "\\raktar.local\backup\HX90\eBooks"
 # robocopy $src $dst /E /FFT /Z /M /SL /SJ /MT /NP /R:10 /W:10 /COPY:DT
 robocopy $src $dst /MIR /FFT /Z /M /SL /SJ /MT /NP /R:10 /W:10 /COPY:DT
 #endregion
